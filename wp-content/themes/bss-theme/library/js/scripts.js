3aecbe0cbb2bf9b03065b3e8abc6c82b9adaa298
長沢 <a_nagasawa@bss-j.co.jp>
2024-08-22 18:35:14 +0900

    分かりにくいコメントを翻訳

--- wp-content/themes/bss-theme/library/js/scripts.js
+++ wp-content/themes/bss-theme/library/js/scripts.js
@@ -1,120 +1,112 @@
 /*
- * Bones Scripts File
- * Author: Eddie Machado
+ * Bones スクリプトファイル
  *
- * This file should contain any js scripts you want to add to the site.
- * Instead of calling it in the header or throwing it inside wp_head()
- * this file will be called automatically in the footer so as not to
- * slow the page load.
- *
- * There are a lot of example functions and tools in here. If you don't
- * need any of it, just remove it. They are meant to be helpers and are
- * not required. It's your world baby, you can do whatever you want.
+ * ここには多くの例やツールが含まれています。必要ない場合は削除してください。
+ * これらはヘルパーとして意図されており、必須ではありません。
 */
 
 
 /*
- * Get Viewport Dimensions
- * returns object with viewport dimensions to match css in width and height properties
- * ( source: http://andylangton.co.uk/blog/development/get-viewport-size-width-and-height-javascript )
+ * ビューポートの寸法を取得
+ * ビューポートの寸法をオブジェクトとして返し、CSSの幅と高さのプロパティに一致させます
+ * ( 出典: http://andylangton.co.uk/blog/development/get-viewport-size-width-and-height-javascript )
 */
 function updateViewportDimensions() {
-	var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
-	return { width:x,height:y };
+  var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
+  return { width:x,height:y };
 }
-// setting the viewport width
+// ビューポートの幅を設定
 var viewport = updateViewportDimensions();
 
 
 /*
- * Throttle Resize-triggered Events
- * Wrap your actions in this function to throttle the frequency of firing them off, for better performance, esp. on mobile.
- * ( source: http://stackoverflow.com/questions/2854407/javascript-jquery-window-resize-how-to-fire-after-the-resize-is-completed )
+* リサイズトリガーイベントのスロットル
+* この関数内にアクションをラップして、発火頻度をスロットルし、特にモバイルでのパフォーマンスを向上させます。
+* ( 出典: http://stackoverflow.com/questions/2854407/javascript-jquery-window-resize-how-to-fire-after-the-resize-is-completed )
 */
 var waitForFinalEvent = (function () {
-	var timers = {};
-	return function (callback, ms, uniqueId) {
-		if (!uniqueId) { uniqueId = "Don't call this twice without a uniqueId"; }
-		if (timers[uniqueId]) { clearTimeout (timers[uniqueId]); }
-		timers[uniqueId] = setTimeout(callback, ms);
-	};
+  var timers = {};
+  return function (callback, ms, uniqueId) {
+      if (!uniqueId) { uniqueId = "一意のIDなしでこれを2回呼び出さないでください"; }
+      if (timers[uniqueId]) { clearTimeout (timers[uniqueId]); }
+      timers[uniqueId] = setTimeout(callback, ms);
+  };
 })();
 
-// how long to wait before deciding the resize has stopped, in ms. Around 50-100 should work ok.
+// リサイズが停止したと判断するまでの待機時間（ミリ秒）。50〜100程度が適切です。
 var timeToWaitForLast = 100;
 
 
 /*
- * Here's an example so you can see how we're using the above function
- *
- * This is commented out so it won't work, but you can copy it and
- * remove the comments.
- *
- *
- *
- * If we want to only do it on a certain page, we can setup checks so we do it
- * as efficient as possible.
- *
- * if( typeof is_home === "undefined" ) var is_home = $('body').hasClass('home');
- *
- * This once checks to see if you're on the home page based on the body class
- * We can then use that check to perform actions on the home page only
- *
- * When the window is resized, we perform this function
- * $(window).resize(function () {
- *
- *    // if we're on the home page, we wait the set amount (in function above) then fire the function
- *    if( is_home ) { waitForFinalEvent( function() {
- *
- *	// update the viewport, in case the window size has changed
- *	viewport = updateViewportDimensions();
- *
- *      // if we're above or equal to 768 fire this off
- *      if( viewport.width >= 768 ) {
- *        console.log('On home page and window sized to 768 width or more.');
- *      } else {
- *        // otherwise, let's do this instead
- *        console.log('Not on home page, or window sized to less than 768.');
- *      }
- *
- *    }, timeToWaitForLast, "your-function-identifier-string"); }
- * });
- *
- * Pretty cool huh? You can create functions like this to conditionally load
- * content and other stuff dependent on the viewport.
- * Remember that mobile devices and javascript aren't the best of friends.
- * Keep it light and always make sure the larger viewports are doing the heavy lifting.
- *
+* ここに上記の関数を使用する例があります
+*
+* これはコメントアウトされているので動作しませんが、コピーして
+* コメントを削除することができます。
+*
+*
+*
+* 特定のページでのみ実行したい場合、チェックを設定して
+* できるだけ効率的に実行します。
+*
+* if( typeof is_home === "undefined" ) var is_home = $('body').hasClass('home');
+*
+* これはボディクラスに基づいてホームページにいるかどうかを確認します
+* そのチェックを使用して、ホームページでのみアクションを実行できます
+*
+* ウィンドウがリサイズされたとき、この関数を実行します
+* $(window).resize(function () {
+*
+*    // ホームページにいる場合、設定された時間を待ってから関数を発火します
+*    if( is_home ) { waitForFinalEvent( function() {
+*
+*	// ウィンドウサイズが変更された場合に備えてビューポートを更新します
+*	viewport = updateViewportDimensions();
+*
+*      // 768以上の場合、これを発火します
+*      if( viewport.width >= 768 ) {
+*        console.log('ホームページにいて、ウィンドウサイズが幅768以上です。');
+*      } else {
+*        // それ以外の場合、これを実行します
+*        console.log('ホームページではないか、ウィンドウサイズが幅768未満です。');
+*      }
+*
+*    }, timeToWaitForLast, "your-function-identifier-string"); }
+* });
+*
+* かなりクールですね？このような関数を作成して、ビューポートに依存する
+* コンテンツやその他のものを条件付きでロードできます。
+* モバイルデバイスとJavaScriptはあまり相性が良くないことを覚えておいてください。
+* 軽量に保ち、常に大きなビューポートが重い作業を行うようにしてください。
+*
 */
 
 /*
- * We're going to swap out the gravatars.
- * In the functions.php file, you can see we're not loading the gravatar
- * images on mobile to save bandwidth. Once we hit an acceptable viewport
- * then we can swap out those images since they are located in a data attribute.
+* グラバターを交換します。
+* functions.phpファイルでは、帯域幅を節約するためにモバイルでグラバター画像を読み込んでいません。
+* 受け入れ可能なビューポートに達したら、データ属性にあるこれらの画像を交換できます。
 */
 function loadGravatars() {
-  // set the viewport using the function above
-  viewport = updateViewportDimensions();
-  // if the viewport is tablet or larger, we load in the gravatars
-  if (viewport.width >= 768) {
-  jQuery('.comment img[data-gravatar]').each(function(){
-    jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
-  });
-	}
-} // end function
+// 上記の関数を使用してビューポートを設定
+viewport = updateViewportDimensions();
+// ビューポートがタブレット以上の場合、グラバターを読み込みます
+if (viewport.width >= 768) {
+jQuery('.comment img[data-gravatar]').each(function(){
+  jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
+});
+  }
+} // 関数の終了
 
 
 /*
- * Put all your regular jQuery in here.
+* すべての通常のjQueryをここに入れます。
 */
 jQuery(document).ready(function($) {
 
-  /*
-   * Let's fire off the gravatar function
-   * You can remove this if you don't need it
-  */
-  loadGravatars();
+/*
+ * グラバター関数を発火させます
+ * 必要ない場合はこれを削除できます
+*/
+loadGravatars();
 
 
-}); /* end of as page load scripts */
+}); /* ページロードスクリプトの終了 */
