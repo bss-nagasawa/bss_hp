04afe88762b73cddc2ee8f0fcc81e14593250a3e
長沢 <a_nagasawa@bss-j.co.jp>
2024-08-23 13:54:29 +0900

    テーマファイルの修正｜無駄なHTML削除

--- wp-content/themes/bss-theme/search.php
+++ wp-content/themes/bss-theme/search.php
@@ -1,46 +1,18 @@
 <?php get_header(); ?>
 
-<div id="content">
+<div class="conteiner">
 
-	<div id="inner-content" class="wrap cf">
+	<div id="inner-content" class="wrap">
 
-		<main id="main" class="m-all t-2of3 d-5of7 cf" role="main">
-			<h1 class="archive-title"><span><?php _e('Search Results for:', 'bonestheme'); ?></span> <?php echo esc_attr(get_search_query()); ?></h1>
+		<main id="search-content" class="main" role="main">
+			<h1 class="archive-title"><?php echo esc_attr(get_search_query()); ?></h1>
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-					<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
+					<article id="post-<?php the_ID(); ?>" role="article">
 
-						<header class="entry-header article-header">
-
-							<h3 class="search-title entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-
-							<p class="byline entry-meta vcard">
-								<?php printf(
-									__('Posted %1$s by %2$s', 'bonestheme'),
-									/* the time the post was published */
-									'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-									/* the author of the post */
-									'<span class="by">by</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link(get_the_author_meta('ID')) . '</span>'
-								); ?>
-							</p>
-
-						</header>
-
-						<section class="entry-content">
-							<?php the_excerpt('<span class="read-more">' . __('Read more &raquo;', 'bonestheme') . '</span>'); ?>
-
-						</section>
-
-						<footer class="article-footer">
-
-							<?php if (get_the_category_list(', ') != ''): ?>
-								<?php printf(__('Filed under: %1$s', 'bonestheme'), get_the_category_list(', ')); ?>
-							<?php endif; ?>
-
-							<?php the_tags('<p class="tags"><span class="tags-title">' . __('Tags:', 'bonestheme') . '</span> ', ', ', '</p>'); ?>
-
-						</footer> <!-- end article footer -->
+						<?php the_title(); ?>
+						<?php the_excerpt('<span class="read-more">' . __('Read more &raquo;', 'bonestheme') . '</span>'); ?>
 
 					</article>
 
@@ -50,23 +22,13 @@
 
 			<?php else : ?>
 
-				<article id="post-not-found" class="hentry cf">
-					<header class="article-header">
-						<h1><?php _e('Sorry, No Results.', 'bonestheme'); ?></h1>
-					</header>
-					<section class="entry-content">
-						<p><?php _e('Try your search again.', 'bonestheme'); ?></p>
-					</section>
-					<footer class="article-footer">
-						<p><?php _e('This is the error message in the search.php template.', 'bonestheme'); ?></p>
-					</footer>
+				<article id="post-not-found" class="hentry">
+					お探しのキーワードで該当ページが見つかりませんでした。
 				</article>
 
 			<?php endif; ?>
 
 		</main>
-
-		<?php get_sidebar(); ?>
 
 	</div>
 
