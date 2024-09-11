08d268104fe6d462e707d0a43f6fa104ba040e6a
長沢 <a_nagasawa@bss-j.co.jp>
2024-09-11 18:16:59 +0900

    不要なテーマファイルの削除・会社概要ページのテキスト挿入

--- wp-content/themes/bss-theme/parts/company/parts-company-history.php
+++ wp-content/themes/bss-theme/parts/company/parts-company-history.php
@@ -1,4 +1,4 @@
 <div id="company-history" class="content-history">
-    <h2>沿革</h2>
-    <p>ここに沿革の内容を記載します。</p>
+    <h3>沿革</h3>
+    <?php echo do_shortcode('[table id=7 /]') ?>
 </div><!-- /#company-history -->
