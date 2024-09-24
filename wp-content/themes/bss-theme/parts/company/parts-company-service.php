4318b754d513ea030e2f3e26ba782fdff245211e
長沢 <a_nagasawa@bss-j.co.jp>
2024-09-24 17:54:40 +0900

    会社概要ページコンテンツ作成

--- wp-content/themes/bss-theme/parts/company/parts-company-service.php
+++ wp-content/themes/bss-theme/parts/company/parts-company-service.php
@@ -1,4 +1,39 @@
 <div id="company-service" class="content-service">
-    <h2>事業内容</h2>
-    <p>ここに事業内容の内容を記載します。</p>
+    <h3>事業内容</h3>
+    <h4>Business Content</h4>
+    <div>
+        <dl>
+            <dt>
+                <span>SES</span>
+                <sapn>System Engineering Service</sapn>
+            </dt>
+            <dd>
+                お客様先に常駐し、システム設計・開発をはじめ、インフラ関連幅広く対応できております。金融、物流を中心に、大手SIerからベンダー企業まで様々なお客様から案件を頂いております。Java、.NETなどWEB系や、スマートフォン開発（アジャイル）を得意としており、今後は、下記分野を技術方向性として確立しつつあります。
+                <ul>
+                    <li>
+                        クラウドソリューション
+                        （AWS、Azure、Google Cloud）
+                    </li>
+                    <li>
+                        CRM
+                        （Salesforce、SAP）
+                    </li>
+                    <li>
+                        人工知能
+                        （AI/Python）
+                    </li>
+                </ul>
+            </dd>
+        </dl>
+        <dl>
+            <dt>
+                <span>受託開発</span>
+                <span>Entrusted development</span>
+            </dt>
+            <dd>
+                当社では、システムのメンテナンス保守から、ニーズのヒアリングからのフルソリューションまで手掛けております。業務システム開発や、WEBサービス、モバイルアプリなどの各種ソフトウェアの開発について、ニーズヒアリングから、要件定義、設計、プログラミング、テスト、運用の各工程に合わせたご要望を踏まえてハイスキルエンジニアが対応します。また、オフショアを利用するシステム開発も実績あります。契約形態は、ラボ型、一括請負型からお客様のニーズに合わせてお選び頂けます。
+                <img src="<?php echo theme_image_directory(); ?>/company/business.png">
+            </dd>
+        </dl>
+    </div>
 </div><!-- /#company-service -->
