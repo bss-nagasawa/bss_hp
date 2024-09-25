b35427ac615c4e3620d5a916c1f52ff0a2711328
長沢 <a_nagasawa@bss-j.co.jp>
2024-09-25 16:18:18 +0900

    home>aboutまでコーディング

--- wp-content/themes/bss-theme/parts/home/parts-home-about.php
+++ wp-content/themes/bss-theme/parts/home/parts-home-about.php
@@ -1,23 +1,25 @@
-<div class="content-header">
+<div class="content-header contentHeader-spacing">
     <h3 class="content-header-title">BSSとは</h3>
     <h4 class="content-header-ruby">About Us</h4>
 </div>
 <div class="content-body content-about">
-    <p>エンジニアのキャリアアップを全面サポートします。エンジニアの成長は企業の成長という考えのもと、
-        案件選択や定期的なキャリアサポート面談をはじめキャリアアップしやすい環境づくりに注力。業界最高水準の還元率を誇っており、社員には報酬額はもちろん様々な福利厚生でバックアップ！</p>
+    <p class="uk-text-small uk-text-center uk-margin-large-bottom line2">エンジニアのキャリアアップを全面サポートします。<br>
+        エンジニアの成長は企業の成長という考えのもと、<br>案件選択や定期的なキャリアサポート面談をはじめキャリアアップしやすい環境づくりに注力。<br>
+        業界最高水準の還元率を誇っており、社員には報酬額はもちろん様々な福利厚生でバックアップ！
+    </p>
     <div class="content-about-images">
-        <ul class="images-childs">
-            <li>
-                Vision
-                <img src="<?php echo theme_image_directory(); ?>/vision.png">
-                BSSが目指す世界
-                今までの自分を超える未来
+        <ul class="images-childs uk-flex uk-flex-wrap uk-flex-center uk-text-center">
+            <li class="images-childs-card uk-background-default uk-margin-medium-right">
+                <p class="card-title tc-em">Vision</p>
+                <img src="<?php echo theme_image_directory(); ?>/home/vision.png" alt="未来を見据えるビジネスマン" class="uk-margin-auto">
+                <h4 class="card-head uk-margin-small-bottom">BSSが<span class="tc-sem">目指す世界</span></h4>
+                <p class="uk-text-small">今までの自分を超える未来</p>
             </li>
-            <li>
-                Mission
-                <img src="<?php echo theme_image_directory(); ?>/mission.png">
-                そのために実現すること
-                社員第一をモットーに一人ひとりに寄り添ってキャリアを形成
+            <li class="images-childs-card uk-background-default uk-margin-medium-left">
+                <p class="card-title tc-em">Mission</p>
+                <img src="<?php echo theme_image_directory(); ?>/home/mission.png" alt="寄り添うコンサルタント" class="uk-margin-auto">
+                <h4 class="card-head uk-margin-small-bottom">そのために<span class="tc-sem">実現</span>すること</h4>
+                <p class="uk-text-small">社員第一をモットーに<br>一人ひとりに寄り添ってキャリアを形成</p>
             </li>
         </ul>
     </div>
