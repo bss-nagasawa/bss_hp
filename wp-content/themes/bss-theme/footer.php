4e39194572b9c5b5a1dd23abf3e6c3ec7f9fb974
長沢 <a_nagasawa@bss-j.co.jp>
2024-09-27 18:17:53 +0900

    footer完レスポンシブ対応開始

--- wp-content/themes/bss-theme/footer.php
+++ wp-content/themes/bss-theme/footer.php
@@ -1,16 +1,23 @@
-			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-				<div id="" class="footer-content">
-					<?php get_template_part('parts/footer/parts-footer-access'); ?>
-					<?php
-					wp_nav_menu(array(
-						'theme_location' => 'footer',
-						'container' => false,
-						'menu_class' => 'footer',
-						'items_wrap' => '%3$s', // <ul>タグを出力しない
-						'walker' => new Walker_Nav_Menu_No_UL(), // カスタムウォーカーを使用
-					));
-					?>
-
+			<footer id="footer" class="footer sp" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+				<div class="footer-content">
+					<div class="inner">
+						<div class="footer-container uk-flex uk-flex-wrap ">
+							<div class="footer-access">
+								<?php get_template_part('parts/footer/parts-footer-access'); ?>
+							</div>
+							<div class="footer-links">
+								<?php
+								wp_nav_menu(array(
+									'theme_location' => 'footer',
+									'container' => false,
+									'menu_class' => 'footer',
+									'items_wrap' => '%3$s', // <ul>タグを出力しない
+									'walker' => new Walker_Nav_Menu_No_UL(), // カスタムウォーカーを使用
+								));
+								?>
+							</div>
+						</div>
+					</div>
 				</div>
 				<?php get_template_part('parts/footer/parts-footer-copy'); ?>
 			</footer>
