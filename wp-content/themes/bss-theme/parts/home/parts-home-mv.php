6dbb1aa22d0e1200fc3283cd77c3a229635d3110
長沢 <a_nagasawa@bss-j.co.jp>
2024-10-01 19:20:55 +0900

    レスポンシブ対応　インタビューまで

--- wp-content/themes/bss-theme/parts/home/parts-home-mv.php
+++ wp-content/themes/bss-theme/parts/home/parts-home-mv.php
@@ -1,8 +1,8 @@
 <div class="mv-inner">
     <div class="mv-inner-left">
         <div class="mv-left-content">
-            <h2 class="mv-headline tc-em">今までの自分を超える<span class="tc-sem">未来</span>を<span class="tc-sem">創造</span>する！</h2>
-            <h3 class="mv-description">社員のための会社・社員の成長と幸せを追求する<br>10年キャリアプランを立てロードマップを明確化！</h3>
+            <h2 class="mv-headline tc-em">今までの自分を超える<br class="sp"><span class="tc-sem">未来</span>を<span class="tc-sem">創造</span>する！</h2>
+            <h3 class="mv-description">社員のための<br class="sp">会社・社員の成長と幸せを追求する<br>10年キャリアプランを立て<br class="sp">ロードマップを明確化！</h3>
             <div class="uk-flex mv-btn-wrap">
                 <a href="#" class="btn btn-mc tc-white">採用情報</a>
                 <a href="#" class="btn btn-sc tc-white">エントリー</a>
