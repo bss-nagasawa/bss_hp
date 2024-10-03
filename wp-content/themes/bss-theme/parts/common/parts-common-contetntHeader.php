3de74123049ccacbdb5455c14be87296b0871545
長沢 <a_nagasawa@bss-j.co.jp>
2024-10-03 18:53:11 +0900

    会社概要PC

--- wp-content/themes/bss-theme/parts/common/parts-common-contetntHeader.php
+++ wp-content/themes/bss-theme/parts/common/parts-common-contetntHeader.php
@@ -1,7 +1,29 @@
 <?php
 global $post;
 $page_slug = $post->post_name;
+$page_title = $post->post_title;
+if ($page_slug === 'company') {
+    $content_title = 'Company';
+    $title_class = 'main-content-header';
+} elseif ($page_slug === 'recruit') {
+    $content_title = 'Recruit';
+    $title_class = 'main-content-header';
+} else {
+    $content_title = $page_title;
+    $title_class = 'common-header';
+}
 ?>
-<div class="content-header-image">
-    <img src="<?php echo theme_image_directory(); ?>/content-header/<?php echo $page_slug; ?>.png">
-</div>
+<?php if ($title_class == 'common-header'): ?>
+    <div class="page-header-wrap <?php echo $title_class; ?>">
+        <div class="page-header-inner">
+            <h1 class="page-headline"><?php echo $content_title; ?></h1>
+        </div>
+    </div>
+<?php else: ?>
+    <div class="page-header-wrap <?php echo $title_class; ?>">
+        <div class="page-header-inner inner uk-flex">
+            <h2 class="page-headline-ruby"><?php echo $page_title; ?></h2>
+            <h1 class="page-headline tc-em"><?php echo $content_title; ?></h1>
+        </div>
+    </div>
+<?php endif; ?>
--- wp-content/themes/bss-theme/parts/company/parts-company-organization.php
+++ wp-content/themes/bss-theme/parts/company/parts-company-organization.php
@@ -1,7 +1,10 @@
 <div id="company-organization" class="content-organization">
-    <h3>事業内容</h3>
-    <h4>Business Content</h4>
-    <div>
+    <div class="content-header contentHeader-spacing">
+        <h3 class="content-header-title">会社組織図</h3>
+        <h4 class="content-header-ruby">Organization Chart</h4>
+    </div>
+
+    <div class="inner content-inner">
         <img src="<?php echo theme_image_directory(); ?>/company/organization.png">
     </div>
 </div><!-- /#company-organization -->
